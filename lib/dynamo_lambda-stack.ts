6c25ac2ab051c5ea4a1778ac472fdabaf5d28bf3
John Brian <njugunajb96@gmail.com>
2023-02-17 11:24:04 +0300

    add rules

--- lib/dynamo_lambda-stack.ts
+++ lib/dynamo_lambda-stack.ts
@@ -1,16 +1,70 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import * as iam from "aws-cdk-lib/aws-iam";
+import * as appautoscaling from "aws-cdk-lib/aws-applicationautoscaling";
 
 export class DynamoLambdaStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    //create table
+    const table = new dynamodb.Table(this, id, {
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      // readCapacity: 1,
+      // writeCapacity: 1,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      partitionKey: { name: "id", type: dynamodb.AttributeType.STRING },
+      sortKey: { name: "createdAt", type: dynamodb.AttributeType.NUMBER },
+      pointInTimeRecovery: true,
+      tableName: "john",
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'DynamoLambdaQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
+    console.log("Table name =>", table.tableName);
+    console.log("Table arn =>", table.tableArn);
+
+    //add local secondary index
+    table.addLocalSecondaryIndex({
+      indexName: "statusIndex",
+      sortKey: { name: "status", type: dynamodb.AttributeType.STRING },
+      projectionType: dynamodb.ProjectionType.ALL,
+    });
+
+    //grant permissions
+    table.grantReadData(new iam.AccountRootPrincipal());
+
+    //grant read permission to lambda
+    // table.grantReadData(lambdaName);
+
+    //grant specific permission to lambda
+    // table.grant(lambdaName, ["dynamodb:Query"]);
+
+    /**
+     * Auto Scaling Configurations for Provisioned databases
+     *
+     */
+
+    //configure autoscaling on table
+    // const writeAutoScalng = table.autoScaleWriteCapacity({
+    //   maxCapacity: 2,
+    //   minCapacity: 1,
+    // });
+
+    //scale up when write capacity hits 75%
+    // writeAutoScalng.scaleOnUtilization({
+    //   targetUtilizationPercent: 75,
+    // });
+
+    //scale up at 9am
+    // writeAutoScalng.scaleOnSchedule("scale-up", {
+    //   schedule: appautoscaling.Schedule.cron({ hour: "9", minute: "0" }),
+    //   minCapacity: 2,
+    // });
+
+    //scale down in the afternoon
+    // writeAutoScalng.scaleOnSchedule("scale-down", {
+    //   schedule: appautoscaling.Schedule.cron({ hour: "14", minute: "0" }),
+    //   maxCapacity: 2,
     // });
   }
 }
